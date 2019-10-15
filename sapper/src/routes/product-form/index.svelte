352c392ca9626bf1bdbf5f73a768f0f8284eedb3
crisgol <cristano@golsistemas.com.br>
2019-10-15 08:43:43 -0400

    Renderizando rotas

---
+++ sapper/src/routes/product-form/index.svelte
@@ -0,0 +1,103 @@
+<script context="module">
+    export function preload({ params, query }) {
+      /*return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+        return { posts };
+
+      });
+      */
+      if (params.id) {
+        /*this.productService.getProduct(params.id)
+        .subscribe(
+          res => {
+            console.log(res);
+            this.product = res;
+            this.edit = true;
+          },
+          err => console.log(err)
+        )*/
+      }
+    }
+  </script>
+  
+  <script>
+    import { onMount } from 'svelte';
+   // import { basePath } from '../../config';
+    //import { ProductService } from '../../services/product.service';
+   // import { Product } from '../../interfaces/Product';
+   
+    let product = {
+      name: '',
+      description: '',
+      imageURL: '',
+      price: 0
+    };
+    
+    let edit= false;
+
+
+    onMount(() => {
+       /* fetch(basePath + '/api/SampleData/WeatherForecasts')
+            .then(response => response.json())
+            .then(data => {
+                forecasts = data;
+            });
+
+            this.fetch(`blog.json`)
+            .then(r => r.json())
+            .then(posts => {
+               return { posts };
+            } 
+            */
+      //this.getProducts();    
+    });
+
+    function submitProduct() {
+    /*this.productService.createProduct(this.product)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/']);
+        },
+        err => console.log(err)
+      )*/
+  }
+
+  function updateProduct() {
+    /*delete this.product.createdAt;
+    this.productService.updateProduct(this.product._id, this.product)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/product'])
+        },
+        err => console.log(err)
+      )*/
+  }
+
+</script>
+
+<div class="row">
+  <div class="col-sm-4 offset-sm-4">
+    <div class="card card-body">
+
+      <form on:submit={edit ? updateProduct() : submitProduct()}>
+
+        <div class="form-group">
+          <input type="text" name="name" bind:value="{product.name}" class="form-control" placeholder="Product Name" >
+        </div>
+        <div class="form-group">
+          <input type="text" name="description" bind:value="{product.description}" class="form-control" placeholder="Description">
+        </div>
+        <div class="form-group">
+          <input type="text" name="price" bind:value="{product.price}" class="form-control" placeholder="Price">
+        </div>
+        <div class="form-group">
+          <input type="text" name="imageURL" bind:value="{product.imageURL}" class="form-control" placeholder="Image URL">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">
+          Save
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ sapper/src/routes/product-list/index.svelte
@@ -0,0 +1,109 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
+<script>
+  import { onMount } from 'svelte';
+ // import { basePath } from '../../config';
+  //import { ProductService } from '../../services/product.service';
+ // import { Product } from '../../interfaces/Product';
+
+  export let products=[];
+
+  onMount(() => {
+       /* fetch(basePath + '/api/SampleData/WeatherForecasts')
+            .then(response => response.json())
+            .then(data => {
+                forecasts = data;
+            });
+
+            this.fetch(`blog.json`)
+            .then(r => r.json())
+            .then(posts => {
+               return { posts };
+            } 
+            */
+            products=getProducts();  
+            console.log(products);  
+            
+    });
+
+  function getProducts(){
+    /*this.productService.getProducts()
+      .subscribe(
+        res => this.products = res,
+        err => console.log(err)
+      )*/
+      console.log('getProducts');
+      return [{
+        _id: '1',
+         name: 'TABLET',
+         description: 'TABLET 180',
+         price: 100,
+         createdAt: '01/01/2019'
+      },
+      {
+        _id: '2',
+         name: 'PC',
+         description: 'COMPUTADOR 180',
+         price: 150,
+         createdAt: '01/01/2019'
+      }
+    ];
+
+  }
+
+  function deleteProduct(id) {
+    /*this.productService.deleteProduct(id)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.getProducts();
+        },
+        err => console.log(err)
+      )
+      */
+      console.log('produto excluido '+id);
+  }
+
+</script>
+
+{#each products as product}
+<div class="card card-body mb-2">
+  <div class="row">
+    <div class="col-md-8">
+      <ul>
+        <li>
+          id: {product._id}
+        </li>
+        <li>
+          name: {product.name}
+        </li>
+        <li>
+          description: {product.description}
+        </li>
+        <li>
+          price: {product.price}
+        </li>
+        <li>
+          createdAt: {product.createdAt}
+        </li>
+      </ul>
+      <button on:click="{() => deleteProduct(product._id)}" class="btn btn-danger">
+        Delete
+      </button>
+      <button href="/product-form/edit/{product._id}" class="btn btn-secondary">
+        Edit
+      </button>
+    
+    </div>
+    <div class="col-md-4">
+      <!--<img [src]="{product.imageURL}" class="card-img"> -->
+    </div>
+  </div>
+</div>
+{/each}
